c23bd7120680de3515f785ef459f09b6d52b8906
Samarth mishra <samarthmishra46@gmail.com>
2025-12-24 11:15:33 +0530

    readme update

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,15 +3,16 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between">
+      <div className="container p-8 flex justify-between ">
+
         <span>          <Image 
             src="/images/footerLogo.png" 
             alt="Samarth" 
-            className="h-10 w-auto md:h-12 lg:h-14"
-            width={500}
-            height={500}
+            className="h-12 w-auto md:h-20 lg:h-28 "
+            width={900}
+            height={900}
           /></span>
-        <p className="text-slate-600">All rights reserved.</p>
+        
       </div>
     </footer>
   );
